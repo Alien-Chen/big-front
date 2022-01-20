dc878bb252d285c4562b99a7350e15e57f0c6c83
Alien-Chen <522864794@qq.com>
2022-01-20 13:30:32 +0000

    Deploy to Github page [ci skip]

--- js/forget~login~reg.js
+++ js/forget~login~reg.js
@@ -396,7 +396,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _nod
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  BaseUrl: {\n    dev: 'http://localhost:3000',\n    prod: 'xxx'\n  }\n});\n\n//# sourceURL=webpack:///./src/config/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  BaseUrl: {\n    dev: 'http://localhost:3000',\n    prod: 'xxxx'\n  }\n});\n\n//# sourceURL=webpack:///./src/config/index.js?");
 
 /***/ }),
 
